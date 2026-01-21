f4fa2e62edcb6fed5aeba38cd5be0da3eb7e0e09
Jonas Follesø <jonas@follesoe.no>
2026-01-21 21:07:28 +0100

    Initial Commit

---
+++ BikeBonk/BikeBonkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeBonkApp.swift
+//  BikeBonk
+//
+//  Created by Jonas Follesø on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BikeBonkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeBonkTests/BikeBonkTests.swift
@@ -0,0 +1,17 @@
+//
+//  BikeBonkTests.swift
+//  BikeBonkTests
+//
+//  Created by Jonas Follesø on 21/01/2026.
+//
+
+import Testing
+@testable import BikeBonk
+
+struct BikeBonkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
